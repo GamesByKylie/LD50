1152fdf3dc15493362ec3917113b847664e86bab
Kylie <kr.gilde@gmail.com>
2022-04-02 13:54:38 -0400

    Scene loading and placeholder main menu, gameplay, and game over scenes

---
+++ Ludum50/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    //There's no need for additive loading, so we can just go
+    //Also, because this is a small project unlikely to be expanded, we can hard-code the indices
+    //Main menu is 0, main gameplay is 1, game over is 2, and that's it
+
+    public void MainGameScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GameOverScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void StartMenuScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}

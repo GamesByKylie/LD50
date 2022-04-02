e2d107e8b8b199bbf160026f9c636ddc2d990457
Kylie <kr.gilde@gmail.com>
2022-04-02 17:19:43 -0400

    Basic game layout and Sisyphus prototype

---
+++ Ludum50/Assets/Scripts/Sisyphus.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Sisyphus : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+{
+    public Transform startPos;
+    public Transform endPos;
+    public float initialSpeed;
+    public float playerDragSpeed;
+    public float warningDistance = 10f;
+    public float loseDistance = 0.1f;
+    public GameObject warningFX;
+
+    private float t = 0;
+    private bool gameOver = false;
+    private bool isHeld = false;
+
+    private void Start()
+    {
+        transform.position = startPos.position;
+    }
+
+    private void Update()
+    {
+        if (!gameOver && !isHeld)
+        {
+            if (DistanceToEnd() > loseDistance)
+            {
+                if (DistanceToEnd() < warningDistance)
+                {
+                    warningFX.SetActive(true);
+                }
+                else
+                {
+                    if (warningFX.activeSelf)
+                    {
+                        warningFX.SetActive(false);
+                    }
+                }
+
+                transform.position = Vector3.Lerp(startPos.position, endPos.position, t);
+                t += Time.deltaTime * initialSpeed;
+            }
+            else
+            {
+                GameOver();
+            }
+        }
+        else if (!gameOver && isHeld)
+        {
+            transform.position = Vector3.Lerp(startPos.position, endPos.position, t);
+            t -= Time.deltaTime * playerDragSpeed;
+        }
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("being held");
+        isHeld = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("released");
+        isHeld = false;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("pointer left");
+        isHeld = false;
+    }
+
+    private float DistanceToEnd()
+    {
+        return Vector3.Distance(transform.position, endPos.position);
+    }
+
+    private void GameOver()
+    {
+        gameOver = true;
+        Debug.Log("Sisyphus has escaped!");
+    }
+
+}
---
+++ Ludum50/Assets/Scripts/UIGlow.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class UIGlow : MonoBehaviour
+{
+    [Range(0f, 1f)]
+    public float minAlpha;
+    [Range(0f, 1f)]
+    public float maxAlpha;
+
+    public float glowSpeed;
+
+    private Image img;
+    private bool glow = false;
+    private float currentAlpha;
+
+    private float amplitude;
+    private float verticalShift;
+
+    private void Awake()
+    {
+        img = GetComponent<Image>();
+        verticalShift = (maxAlpha + minAlpha) / 2.0f;
+        amplitude = maxAlpha - verticalShift;
+
+        currentAlpha = amplitude * Mathf.Sin(glowSpeed * Time.time) + verticalShift;
+        SetAlphaColor(currentAlpha);
+
+        Debug.Log($"Shifted {verticalShift} up, amplitude {amplitude}");
+    }
+
+    private void OnEnable()
+    {
+        glow = true;
+    }
+
+    private void OnDisable()
+    {
+        glow = false;
+    }
+
+    private void Update()
+    {
+        if (glow)
+        {
+            currentAlpha = amplitude * Mathf.Sin(glowSpeed * Time.time) + verticalShift;
+            SetAlphaColor(currentAlpha);
+        }
+    }
+
+    private void SetAlphaColor(float a)
+    {
+        img.color = new Color(img.color.r, img.color.g, img.color.b, a);
+    }
+}
